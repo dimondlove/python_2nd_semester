43ba80d75cd4029237faa5e37b306b1d280a1965
Yuriy_Rudnev <rudnev2020@gmail.com>
2022-11-28 22:21:41 +0300

    Completed the task

---
+++ task.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov 28 22:12:59 2022
+
+@author: Юрец
+"""
+
+print("Решение кв. уравнения | ax ^ 2 + bx + c = 0")
+
+a = int(input("a="))
+b = int(input("b="))
+c = int(input("c="))
+
+d = b ** 2 - 4*a*c
+
+d
+
+if d < 0:
+    print("Решений нет")
+elif d == 0:
+    x = -b / (2*a)
+    print("x = ", x)
+else:
+    x1 = (-b - d**0.5) / (2*a)
+    x2 = (-b + d**0.5) / (2*a)
+    print("x1 = ", x1, " x2 = ", x2)
