02ffd0a627e6273001f85ffeb8e334d539ca2458
Yuriy_Rudnev <rudnev2020@gmail.com>
2022-12-02 12:47:06 +0300

    Completed the 3rd task in PRZ_3

---
+++ prz_3/task_3.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Dec  2 12:33:57 2022
+
+@author: Юрец
+"""
+
+word = "объектно-ориентированный"
+
+print(word[0:6], word[9:17], word[14:17], word[4:15:5], word[10]+word[12:15]+word[19])
